5621188cbaed7aa3c0e7c211a6c3474bfc47dece
25973 <1@1.com>
2025-05-23 09:36:57 +0800

    测试

---
+++ api/events/event_handlers/create_installed_app_when_app_created.py
@@ -0,0 +1,18 @@
+from events.app_event import app_was_created
+from extensions.ext_database import db
+from models.model import InstalledApp
+from events.record_log import OperationRecordLog
+
+
+@app_was_created.connect
+def handle(sender, **kwargs):
+    """Create an installed app when an app is created."""
+    app = sender
+    installed_app = InstalledApp(
+        tenant_id=app.tenant_id,
+        app_id=app.id,
+        app_owner_tenant_id=app.tenant_id,
+    )
+    db.session.add(installed_app)
+    db.session.commit()
+    OperationRecordLog.Operation_log(app,"created","app")
---
+++ api/events/record_log.py
@@ -0,0 +1,42 @@
+from events.app_event import app_was_created
+from extensions.ext_database import db
+from models.model import InstalledApp,OperationLog
+from models.model import Account
+from flask_login import current_user
+from flask import request
+from datetime import datetime
+
+class OperationRecordLog:
+    @staticmethod
+    def get_client_ip():
+        if request.headers.get("Connecting-IP"):
+            return request.headers.get("Connecting-IP")[0].split(',')[0].strip()
+
+        return request.remote_addr
+
+    def Operation_log(app,action,type):
+        tenant_id = getattr(app, 'tenant', None)  # 如果 app 不存在或 app.tenant 为空，返回 None
+        tenant_id = tenant_id.id if tenant_id else None  # 如果 tenant 存在，取其 id，否则为 None
+
+        #account_id = getattr(current_user, 'id', None)  # 如果 current_user 不存在或没有 id，返回 None
+
+        operation_log = OperationLog(
+            tenant_id = tenant_id,
+            account_id = current_user.id,
+            action=action,
+            content={"metadata":
+                         {"action_by":getattr(current_user, 'name', ''),
+                          "app_id":getattr(app, 'id', None ),
+                          "action_time":datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                          }
+                     },
+            app_id=getattr(app, 'id', None ),
+            app_name=getattr(app, 'name', None ),
+            type=type,
+            created_at=db.func.now(),
+            updated_at=db.func.now(),
+            created_by=getattr(app, 'created_by', None),
+            created_ip = OperationRecordLog.get_client_ip()
+        )
+        db.session.add(operation_log)
+        db.session.commit()
